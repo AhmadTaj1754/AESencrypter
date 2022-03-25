911db4e6a1a6cd80a595d99cf8c1b29770fed86f
AhmadTaj1754 <33974873+AhmadTaj1754@users.noreply.github.com>
2022-03-25 15:03:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,23 @@ setuptools.setup(
     author="Ahmad Taj",
     author_email="Ahmadtaj77@gmail.com",
     description="tool for encrypting files using AES encryption",
-    long_description=long_description,
+    long_description="""AESencrypter is a cross-platform file encryption Python mini-app which can be accessed via The Python Package Index (PyPI).
+
+Install AESencrypter on your system using :
+
+pip3 install AESencrypt
+
+Import AESencrypter into your project using :
+
+import AESencrypter
+
+Example of creating and using instance of AESencrypter:
+
+import AESencrypter
+
+AESencrypter.create()
+
+*works best with Python3""",
     long_description_content_type="text/markdown",
     url="https://github.com/AhmadTaj1754/AESencrypter",
     packages=setuptools.find_packages(),
